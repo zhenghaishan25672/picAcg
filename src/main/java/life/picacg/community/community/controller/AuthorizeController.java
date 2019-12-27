0f024bbefea610e8953023e4bea7c95cd984587b
zhenghaishan <997728584@qq.com>
2019-12-27 15:13:05 +0800

    add check cookies

--- src/main/java/life/picacg/community/community/controller/AuthorizeController.java
+++ src/main/java/life/picacg/community/community/controller/AuthorizeController.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -34,7 +35,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -45,16 +46,18 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessTokenDTO(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
 //        System.out.println(githubUser.getName());
+        //收到数据后往model中填值，再插入数据库
         if(githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
             //登录成功，写cookie和session
-            request.getSession().setAttribute("user",githubUser);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else{
             //登录失败，重新登陆
--- src/main/java/life/picacg/community/community/controller/HelloController.java
+++ src/main/java/life/picacg/community/community/controller/HelloController.java
@@ -1,9 +1,15 @@
 package life.picacg.community.community.controller;
 
+import life.picacg.community.community.mapper.UserMapper;
+import life.picacg.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *
@@ -16,8 +22,22 @@ public class HelloController {
 //        life.picacg.community.community.model.addAttribute("name",name);
 //        return "index";
 //    }
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
